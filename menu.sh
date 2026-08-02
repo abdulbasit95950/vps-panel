7390bf86bf3c058739579fd05879b078530bb914
Abdul Basit <Abdulbasit95950@gmail.com>
2026-08-02 22:08:31 +0500

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-# === MD5 HASH IP APPROVAL SYSTEM ===
-AUTHORIZED_HASHES_URL="https://raw.githubusercontent.com/Abdulbasit95950/vps-panel/main/ips.txt"
-SERVER_IP=$(curl -s https://ipinfo.io/ip || curl -s ifconfig.me)
-
-if [[ -n "$SERVER_IP" ]]; then
-    SERVER_IP_HASH=$(echo -n "$SERVER_IP" | md5sum | awk '{print $1}')
-    CHECK_HASH=$(curl -s "$AUTHORIZED_HASHES_URL" | grep -w "$SERVER_IP_HASH")
-    
-    if [[ -z "$CHECK_HASH" ]]; then
-        echo -e "\033[0;31m[ERROR] Yeh IP ($SERVER_IP) authorized nahi hai!\033[0m"
-        echo -e "\033[1;33mPlease contact developer @Abdulbasit95950 to approve your IP.\033[0m"
-        exit 1
-    fi
-else
-    echo -e "\033[0;31m[ERROR] Server IP detect nahi ho saki!\033[0m"
-    exit 1
-fi
-
 # ==============================================================================
 # Script Name   : RareTriccks VPN Panel (Full Telegram Buttons & Admin Control)
 # ==============================================================================
